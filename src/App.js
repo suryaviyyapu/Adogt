1282021381bb89715080963476cca32c2cececa3
Surya Dhanush <46414110+suryaviyyapu@users.noreply.github.com>
2021-05-05 12:57:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Sample from './components/Sample';
+
+function App() {
+  return (
+    <div className="App">
+      <Sample/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sample.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+
+function Sample() {
+    return (
+        <div>
+            <p>
+                Hello, this is a sample component
+            </p>
+        </div>
+    )
+}
+
+export default Sample
