61650221a48d0076ebdbbe1d8fabdc98184202c7
suryaviyyapu <dhanush.surya01@gmail.com>
2021-05-17 01:06:26 +0530

    Fixed build fails by removing redundant imports

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,21 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import ProfileCard from "./ProfileCard";
 import { Link } from "react-router-dom";
 
-import { retrievePets } from "../actions/adogt";
 import axios from "axios";
 
 const ProfileList = () => {
     const [Pets, setPets] = useState([]);
-    // const [currentIndex, setCurrentIndex] = useState(-1);
-    // const pets = useSelector((state) => state.pets);
 
-    // const dispatch = useDispatch();
-
-    // useEffect(() => {
-        // dispatch(retrievePets());
-    // }, [dispatch]);
     useEffect(() => {
       axios.get(`http://127.0.0.1:8000/api/listing/`)
       .then(res => {
